fb3f34654a53809f8a6de5fe82ddebba5831eec9
edw1nwow <33133870+edw1nwow@users.noreply.github.com>
2020-11-24 22:46:50 +0200

    Updates

---
+++ precache-manifest.034d8f7a0f9896896d0c106daa3f8ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe48e0fcc5f817d61c8b1afdef36318",
+    "url": "/social-network-ts/index.html"
+  },
+  {
+    "revision": "9d9fb3f28bf018cce267",
+    "url": "/social-network-ts/static/css/main.52d7a9ce.chunk.css"
+  },
+  {
+    "revision": "f1832d225fdf96c9145d",
+    "url": "/social-network-ts/static/js/2.b7bacf89.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/social-network-ts/static/js/2.b7bacf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d9fb3f28bf018cce267",
+    "url": "/social-network-ts/static/js/main.154c2824.chunk.js"
+  },
+  {
+    "revision": "e87cb5e6453cc9771cf0",
+    "url": "/social-network-ts/static/js/runtime-main.30a547d1.js"
+  }
+]);
